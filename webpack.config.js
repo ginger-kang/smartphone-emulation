763a905f6be3a072cf1532d3440616f0d50996c0
ginger-kang <kdhoon07@gmail.com>
2021-09-08 20:42:11 +0900

    Feat: webpack configuration and add babel loader

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/main.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [path.resolve(__dirname, "src")],
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
